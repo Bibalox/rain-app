a69f56b909290a3f65e00ac2dcc80b527ec7a24c
JB Bal <bal@abvent.fr>
2024-07-02 15:49:18 +0200

    ➕ Add SASS support

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,26 @@
   <span>Hello world!</span>
 </template>
 
-<style>
+<style lang="scss">
+:root {
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  user-select: none;
+}
 
+html {
+  height: calc(100% + env(safe-area-inset-top));
+  -webkit-tap-highlight-color: transparent;
+}
+
+body {
+  align-items: center;
+  background-color: black;
+  display: flex;
+  height: 100%;
+  justify-content: center;
+  width: 100%;
+  
+  color: white;
+}
 </style>
