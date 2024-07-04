c3440f5d01dc46c0f946703b1b7a970c38133ddf
JB Bal <bal@abvent.fr>
2024-07-04 16:01:07 +0200

    💄 Add a fadeOut prop to the app title

--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
 const props = defineProps<{
-  value: string
+  value: string,
+  fadeOut?: boolean
 }>()
 </script>
 
 <template>
   <h1
-    class="app-title"
+    :class="[
+      'app-title',
+      { 'app-title--fade-out': fadeOut }
+    ]"
     v-text="props.value"
   />
 </template>
@@ -19,5 +23,14 @@ const props = defineProps<{
   font-size: 48px;
   margin: 0;
   text-align: center;
+
+  &--fade-out {
+    animation: fadeOut 5s 5s ease-out both;
+
+    @keyframes fadeOut {
+      from { opacity: 1; }
+      to { opacity: 0; }
+    }
+  }
 }
 </style>
