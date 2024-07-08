4d565e16544ad4de6676e9b90314bbd57972e1e0
JB Bal <bal@abvent.fr>
2024-07-08 17:18:55 +0200

    💄 Minor improvement of the puddle

--- src/components/AppPuddle.vue
+++ src/components/AppPuddle.vue
@@ -1,6 +1,3 @@
-<script setup lang="ts">
-</script>
-
 <template>
   <div class="app-puddle">
     <div
@@ -24,7 +21,6 @@
     border: 2px solid white;
     border-radius: 50%;
     box-shadow: inset 0px 0px 20px 2px rgba(255, 255, 255, 0.3);
-    height: 150px;
     opacity: 0;
     position: absolute;
     transform: translate(-100px, -100px);
@@ -35,22 +31,25 @@
       $left: (random(2001) - 1) * 0.05%;
       $delay: (random(6001) - 1) * 0.005s;
       $duration: 4s;
+      $size: random(110) + 40px;
 
       &:nth-of-type(#{$i}) {
         animation: splash $duration $delay infinite cubic-bezier(0.25, 1, 0.5, 1);
         left: $left;
         top: $top;
+        height: $size;
+        width: $size;
       }
-    }
 
-    @keyframes splash {
-      from {
-        opacity: 0.5;
-        transform: scale(0);
-      }
-      to {
-        opacity: 0;
-        transform: scale(1);
+      @keyframes splash {
+        from {
+          opacity: 0.4;
+          transform: scale(0);
+        }
+        to {
+          opacity: 0;
+          transform: scale(1);
+        }
       }
     }
   }
