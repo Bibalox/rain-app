9098ca053b49f32612ea48503870cd7a7e18123e
JB Bal <bal@abvent.fr>
2024-07-16 11:31:49 +0200

    🔊 Add a log to the service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,48 +6,31 @@ const filesToCache = [
   '/sounds/gentle-rain_30-minutes.m4a'
 ]
 
-// self.addEventListener('install', e => {
-//   console.log('[Service Worker] Install')
-//   e.waitUntil(
-//     (async () => {
-//       const cache = await caches.open(cacheName)
-//       console.log('[Service Worker] Caching all: app shell and content')
-//       await cache.addAll(contentToCache)
-//     })()
-//   )
-// })
-
-// self.addEventListener('fetch', e => {
-//   e.respondWith(
-//     (async () => {
-//       const r = await caches.match(e.request)
-//       console.log(`[Service Worker] Fetching resource: ${e.request.url}`)
-//       if (r) {
-//         return r
-//       }
-//       const response = await fetch(e.request)
-//       const cache = await caches.open(cacheName)
-//       console.log(`[Service Worker] Caching new resource: ${e.request.url}`)
-//       cache.put(e.request, response.clone())
-//       return response
-//     })()
-//   )
-// })
-
-self.addEventListener('install', function(e) {
+self.addEventListener('install', e => {
+  console.log('[Service Worker] Install')
   e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      console.log('[Service Worker] Caching all: app shell and content')
-      return cache.addAll(filesToCache)
-    })
+    (async () => {
+      const cache = await caches.open(cacheName)
+      console.log('[Service Worker] Start caching')
+      await cache.addAll(filesToCache)
+      console.log('[Service Worker] Start caching')
+    })()
   )
-})  
+})
 
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(function(response) {
+    (async () => {
+      const r = await caches.match(e.request)
       console.log(`[Service Worker] Fetching resource: ${e.request.url}`)
-      return response || fetch(e.request)
-    })
+      if (r) {
+        return r
+      }
+      const response = await fetch(e.request)
+      const cache = await caches.open(cacheName)
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`)
+      cache.put(e.request, response.clone())
+      return response
+    })()
   )
 })
