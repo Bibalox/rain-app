648daefe36ffc2cb0c9df150b66d1115183ac99b
JB Bal <bal@abvent.fr>
2024-07-16 10:19:34 +0200

    🚧 Try to add the cache management

---
+++ public/service-worker.js
@@ -0,0 +1,35 @@
+const cacheName = 'rainy-app'
+const contentToCache = [
+  '/sounds/distant-storm_1-hour.m4a',
+  '/sounds/distant-storm_30-minutes.m4a',
+  '/sounds/gentle-rain_1-hour.m4a',
+  '/sounds/gentle-rain_30-minutes.m4a'
+]
+
+self.addEventListener('install', e => {
+  console.log('[Service Worker] Install')
+  e.waitUntil(
+    (async () => {
+      const cache = await caches.open(cacheName)
+      console.log('[Service Worker] Caching all: app shell and content')
+      await cache.addAll(contentToCache)
+    })()
+  )
+})
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    (async () => {
+      const r = await caches.match(e.request)
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`)
+      if (r) {
+        return r
+      }
+      const response = await fetch(e.request)
+      const cache = await caches.open(cacheName)
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`)
+      cache.put(e.request, response.clone())
+      return response
+    })()
+  )
+})
--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -3,28 +3,25 @@ import { register } from 'register-service-worker'
 if (import.meta.env.PROD) {
   register('/service-worker.js', {
     ready () {
-      console.log(
-        'App is being served from cache by a service worker.\n' +
-        'For more details, visit https://goo.gl/AFskqB'
-      )
+      console.log('[Service Worker] App is being served from cache by a service worker')
     },
     registered () {
-      console.log('Service worker has been registered.')
+      console.log('[Service Worker] Registered.')
     },
     cached () {
-      console.log('Content has been cached for offline use.')
+      console.log('[Service Worker] Content has been cached for offline use.')
     },
     updatefound () {
-      console.log('New content is downloading.')
+      console.log('[Service Worker] New content is downloading.')
     },
     updated () {
-      console.log('New content is available; please refresh.')
+      console.log('[Service Worker] New content is available; please refresh.')
     },
     offline () {
-      console.log('No internet connection found. App is running in offline mode.')
+      console.log('[Service Worker] No internet connection found. App is running in offline mode.')
     },
     error (error) {
-      console.error('Error during service worker registration:', error)
+      console.error('[Service Worker] Error during service worker registration:', error)
     }
   })
 }
