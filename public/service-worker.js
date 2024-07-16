5b3fd9c9ea9477ebe7ae463aae566aa29b626f8b
JB Bal <bal@abvent.fr>
2024-07-16 11:06:33 +0200

    🚧 Try to make the cache work on Safari

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,35 +1,53 @@
 const cacheName = 'rainy-app'
-const contentToCache = [
+const filesToCache = [
   '/sounds/distant-storm_1-hour.m4a',
   '/sounds/distant-storm_30-minutes.m4a',
   '/sounds/gentle-rain_1-hour.m4a',
   '/sounds/gentle-rain_30-minutes.m4a'
 ]
 
-self.addEventListener('install', e => {
-  console.log('[Service Worker] Install')
-  e.waitUntil(
-    (async () => {
-      const cache = await caches.open(cacheName)
-      console.log('[Service Worker] Caching all: app shell and content')
-      await cache.addAll(contentToCache)
-    })()
-  )
-})
+// self.addEventListener('install', e => {
+//   console.log('[Service Worker] Install')
+//   e.waitUntil(
+//     (async () => {
+//       const cache = await caches.open(cacheName)
+//       console.log('[Service Worker] Caching all: app shell and content')
+//       await cache.addAll(contentToCache)
+//     })()
+//   )
+// })
 
-self.addEventListener('fetch', e => {
+// self.addEventListener('fetch', e => {
+//   e.respondWith(
+//     (async () => {
+//       const r = await caches.match(e.request)
+//       console.log(`[Service Worker] Fetching resource: ${e.request.url}`)
+//       if (r) {
+//         return r
+//       }
+//       const response = await fetch(e.request)
+//       const cache = await caches.open(cacheName)
+//       console.log(`[Service Worker] Caching new resource: ${e.request.url}`)
+//       cache.put(e.request, response.clone())
+//       return response
+//     })()
+//   )
+// })
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[Service Worker] Caching all: app shell and content')
+      return cache.addAll(filesToCache)
+    })
+  )
+})  
+
+self.addEventListener('fetch', function(e) {
   e.respondWith(
-    (async () => {
-      const r = await caches.match(e.request)
+    caches.match(e.request).then(function(response) {
       console.log(`[Service Worker] Fetching resource: ${e.request.url}`)
-      if (r) {
-        return r
-      }
-      const response = await fetch(e.request)
-      const cache = await caches.open(cacheName)
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`)
-      cache.put(e.request, response.clone())
-      return response
-    })()
+      return response || fetch(e.request)
+    })
   )
 })
